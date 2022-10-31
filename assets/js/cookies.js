c95dbe82a44cd8baf464060dc4a3bac4f9f26375
lavreek <lavreeksolacki@yandex.ru>
2022-10-31 17:07:53 +0300

    styles

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
     if ($.cookie('width') == null || $.cookie('width') != $(window).width()) {
+        let width = $(window).width();
 
-        $.cookie('width', $(window).width());
+        $.cookie('width', width);
+
+        // switch(width) {
+        //     case (width > 1440):
+        //         $.cookie('widthType', 'fluid');
+        //         break;
+        //
+        //     case (width > 768):
+        //         $.cookie('widthType', 'lg');
+        //         break;
+        //
+        //     case (width >= 576):
+        //         $.cookie('widthType', 'md');
+        //         break;
+        //
+        //     case (width < 576):
+        //         $.cookie('widthType', 'sm');
+        //         break;
+        // }
+
         window.location.reload();
     }
 })
--- index.php
+++ index.php
@@ -2,14 +2,62 @@
     $base = "<base href=\"{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}/\">";
 
     $media = "";
+    $certificates = "";
+
+    $catalogArray = [
+        "<!--noindex-->Каталог фитингов и труб<!--/noindex-->",
+        "<!--noindex-->Каталог клапанов<!--/noindex-->",
+        "<!--noindex-->Каталог<!--/noindex--> для чистых сред ZCR",
+        "<!--noindex-->Каталог<!--/noindex--> интрументальных манифольдов",
+        "<!--noindex-->Каталог фитингов под приварку<!--/noindex-->",
+    ];
+
+    function putCatalogs($count)
+    {
+        global  $catalogArray;
+
+        $result = "";
+
+        for ($i = 0; $i < $count; $i++) {
+            $result .= "<div class='catalog'><div class='image'><img src='assets/catalog.svg'></div><div class='description'><p class='text'>$catalogArray[$i]</p><a class='link' href='#' download>Скачать</a></div></div>";
+        }
+
+        return $result;
+    }
+
+    function putCertificates($count)
+    {
+        $result = "";
+
+        for ($i = 1; $i <= $count; $i++) {
+            $result .= "<div class='cert'><img src='assets/certs/cert$i.png'></div>";
+        }
+
+        return $result;
+    }
+
 
     if (isset($_COOKIE['width'])) {
         $width = $_COOKIE['width'];
+
         switch ($width) {
-            case (1024 < $width and $width < 1440):
-                $media = "<link rel='stylesheet' href='assets/media/media-1440.css'>";
+            case ($width == "sm"):
+                $certificates = putCertificates(2);
+                break;
+
+            case ($width == "md" or $width == "lg"):
+                $certificates = putCertificates(3);
+                break;
+
+            default:
+                $certificates = putCertificates(4);
                 break;
         }
     }
 
-    include __DIR__ . "/page.html";
+    $catalogs = putCatalogs(5);
+
+
+
+
+include __DIR__ . "/page.html";
